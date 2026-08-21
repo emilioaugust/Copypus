53150484146d4c1159017204fd747ad1e7ac53b0
emilioaugust <kuluev.emil.8@gmail.com>
2026-08-21 19:09:42 +0500

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.emilioaugust.copypus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.2.0"
+        versionCode = 2
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
